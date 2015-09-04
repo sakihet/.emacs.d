3fca80275b0395c56305ca2c00872d19cabbf435
sakihet <sakihet@gmail.com>
2015-09-04 16:49:12 +0900

    use el-get

--- init.el
+++ init.el
@@ -13,3 +13,15 @@
 (delete-selection-mode t)
 (defalias 'yes-or-no-p 'y-or-n-p)
 (electric-pair-mode 1)
+;; el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
