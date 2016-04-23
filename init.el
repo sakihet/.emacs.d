06ea6968945c53943248c3af973035dc4e487e51
saki <sakihet@gmail.com>
2016-04-24 03:29:22 +0900

    add helm key bindings

--- init.el
+++ init.el
@@ -49,6 +49,8 @@
 (load-theme 'tomorrow-day t)
 ;; helm
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
 (helm-mode 1)
 ;; yasnippet
 (yas-global-mode 1)
