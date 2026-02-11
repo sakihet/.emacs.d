aaad2b877a688033eb8f2efebab6c86970060592
saki <sakihet@gmail.com>
2026-02-11 13:57:53 +0900

    update init.el

--- init.el
+++ init.el
@@ -1,131 +1,87 @@
-;; no backup
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; (package-refresh-contents)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company queue rust-mode vertico))
+ '(show-trailing-whitespace t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; backup
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+;; theme
+(load-theme 'modus-operandi t)
+
 ;; display
 (menu-bar-mode 0)
-(show-paren-mode t)
+(global-display-line-numbers-mode t)
 (column-number-mode t)
 (global-hl-line-mode t)
-(custom-set-variables
- '(show-trailing-whitespace t))
-(setq-default indent-tabs-mode nil)
 
 ;; ui
 (delete-selection-mode t)
-(defalias 'yes-or-no-p 'y-or-n-p)
 (electric-pair-mode 1)
 (setq scroll-preserve-screen-position t)
 (setq scroll-conservatively 1)
 (setq scroll-margin 5)
 
-;; window move
-(global-set-key (kbd "M-o") 'other-window)
+;; my function
+(defun reload-init-el ()
+  (interactive)
+  (load-file user-init-file)
+  (message "Reloaded init.el"))
+(defun indent-whole-buffer ()
+  (interactive)
+  (save-excursion
+    (indent-region (point-min) (point-max))
+    )
+  )
 
-;; tramp
-(require 'tramp)
-(setq tramp-default-method "ssh")
+;; company
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  :config
+  (setq company-idle-delay 0
+	company-minimum-prefix-length 1))
 
-;; el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; vertico
+(use-package vertico
+  :custom
+  (vertico-count 20)
+  :init
+  (vertico-mode))
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
-;;; automatic package install
-(setq my-el-get-packages
-      (append
-       '(
-         company-mode
-         company-tern
-         direx
-         emmet-mode
-         flycheck
-         go-mode
-         guide-key
-         helm
-         helm-ag
-         helm-ls-git
-         markdown-mode
-         popwin
-         scala-mode
-         slim-mode
-         tomorrow-theme
-         undo-tree
-         web-mode
-         yaml-mode
-         yasnippet
-         )))
-(el-get 'sync my-el-get-packages)
-
-;; theme
-(load-theme 'tomorrow-day t)
-
-;; company-mode
-(global-company-mode)
-(define-key company-active-map (kbd "M-n") nil)
-(define-key company-active-map (kbd "M-p") nil)
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-
-;; helm
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x C-r") 'helm-recentf)
-(global-set-key (kbd "C-x C-o") 'helm-occur)
-(helm-mode 1)
-(require 'helm-ls-git)
-
-;; yasnippet
-(yas-global-mode 1)
-
-;; emmet-mode
-(require 'emmet-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
-(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
-
-;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;; undo-tree
-(require 'undo-tree)
-(global-undo-tree-mode)
-
-;; popwin
-(require 'popwin)
-(popwin-mode 1)
-
-;; ruby-mode
-(custom-set-variables
- '(ruby-insert-encoding-magic-comment nil))
+;; savehist
+(use-package savehist
+  :init
+  (savehist-mode))
 
 ;; web-mode
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-
-;; go-mode
-(add-hook 'go-mode-hook (lambda()
-                          (setq tab-width 4)))
-
-;; javascript
-(setq js-indent-level 2)
-
-(setq company-tern-property-marker "")
-(add-hook 'js-mode-hook 'tern-mode)
-(add-to-list 'company-backends 'company-tern)
-
-;; flycheck
-(global-flycheck-mode)
+(use-package web-mode
+  :ensure t
+  :mode (("\\.ts\\'" . web-mode)
+         ("\\.js\\'" . web-mode)
+         ("\\.mjs\\'" . web-mode)
+         ("\\.tsx\\'" . web-mode)
+         ("\\.jsx\\'" . web-mode))
+  :config
+  (setq web-mode-content-types-alist
+	'(("jsx" . "\\.js[x]?\\'"))))
